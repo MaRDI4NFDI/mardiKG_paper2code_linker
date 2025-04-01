082e7d1860d4e3daa42140884885bba8b9a8ec73
Tim Conrad <conrad@math.fu-berlin.de>
2025-04-01 19:59:28 +0200

    Workflow is now configured when deploying the flow. Not with a config file / Prefect server variable anymore.

--- workflow_deploy_cloud.py
+++ workflow_deploy_cloud.py
@@ -10,6 +10,16 @@ if __name__ == "__main__":
     ).deploy(
         name="process_papers",
         work_pool_name="CloudWorkerPool",
+        parameters={
+            "data_path": "./data",
+            "db_file": "results.db",
+            "links_file_url": "https://production-media.paperswithcode.com/about/links-between-papers-and-code.json.gz",
+            "batch_size": 1000,
+            "max_workers": 50,
+            "lakefs_url": "https://lake-bioinfmed.zib.de",
+            "lakefs_repo": "mardi-workflows-files",
+            "lakefs_path_and_file": "mardiKG_paper2code_linker/results.db"
+        },
         job_variables={"pip_packages": [
             "boto3",
             "botocore",
--- workflow_main.py
+++ workflow_main.py
@@ -25,34 +25,29 @@ logging.basicConfig(
 #      - mardi-kg-user
 #      - lakefs-user
 #      - lakefs-password
-#  - Create config (in the server UI: Variables -> "+" -> name it "mardikg_paper2code_linker_config"
-#    and copy the content from the "config.json" file
 
 @flow
-def process_papers():
-
+def process_papers(
+    data_path: str,
+    db_file: str,
+    links_file_url: str,
+    batch_size: int,
+    max_workers: int,
+    lakefs_url: str,
+    lakefs_repo: str,
+    lakefs_path_and_file: str
+):
     logger = get_run_logger()
 
-    # Load config at startup
-    logger.info("Loading config...")
-    config = _load_config()
-
     # Set config
-    data_path = config["data_path"]
-    db_path_and_file = str(Path(data_path) / config["db_file"])
-    links_file_url = config["links_file_url"]
-    batch_size = config["batch_size"]
-    max_workers = config["max_workers"]
-    lakefs_url = config["lakefs_url"]
-    lakefs_repo = config["lakefs_repo"]
-    lakefs_path_and_file = config["lakefs_path_and_file"]
+    db_path_and_file = str(Path(data_path) / db_file)
 
     # Check whether data path exists
     Path(data_path).mkdir(parents=True, exist_ok=True)
     logger.info("Ensured data directory exists at: %s", data_path)
 
     # Check whether database file exists
-    if not (Path(data_path) / config["db_file"]).exists():
+    if not (Path(data_path) / db_file).exists():
         logger.warning(f"Database file not found at {db_path_and_file}, trying to download...")
         download_db.submit(
             db_path_and_file=str(db_path_and_file),
@@ -116,52 +111,6 @@ def download_db( db_path_and_file, lakefs_url: str, lakefs_repo: str, lakefs_pat
     logger.info("Successfully saved DB file to '%s'", db_path)
 
 
-def _load_config() -> dict:
-
-    logger = logging.getLogger(__name__)
-    logger.setLevel(logging.INFO)
-    logger.info("Getting config...")
-
-    test_key = "data_path"
-    local_file = "config.json"
-    prefect_var_name = "mardikg_paper2code_linker_config"
-
-    # Try Prefect Variable first
-    try:
-        config = Variable.get(prefect_var_name)
-        if config is None:
-            raise ValueError(f"Prefect variable '{prefect_var_name}' is not set.")
-
-        if not isinstance(config, dict):
-            raise TypeError(f"Prefect variable '{prefect_var_name}' must be a dict (got {type(config)}).")
-
-        if test_key not in config:
-            raise KeyError(f"Key '{test_key}' not found in config from Prefect variable '{prefect_var_name}'.")
-
-        logger.info(f"Loaded config from Prefect variable '{prefect_var_name}'.")
-        return config
-
-    except Exception as e:
-        logger.warning(f"Failed to load config from Prefect variable '{prefect_var_name}': {e}")
-
-
-    # Try local file as fallback
-    try:
-        with open("config.json", "r") as f:
-            config = json.load(f)
-        if test_key not in config:
-            raise KeyError(f"Key '{test_key}' not found in config from local file '{local_file}'.")
-        logger.info(f"Loaded config from local file '{local_file}'")
-        return config
-
-    except Exception as e:
-        logger.error(f"Failed to load config from local file '{local_file}': {e}")
-
-    # If all fails
-    raise RuntimeError(
-        f"Could not load configuration. Tried Prefect variable '{prefect_var_name}' and local file '{local_file}'."
-    )
-
 def _read_lakefs_credentials(path: str = "secrets.conf") -> Optional[Dict[str, str]]:
     """Read user credentials either from prefect server (lakefs-user / lakefs-password)
     or from a secrets file.
@@ -209,4 +158,13 @@ def _read_lakefs_credentials_from_prefect() -> Optional[Dict[str, str]]:
 
 
 if __name__ == "__main__":
-    process_papers()
+    process_papers(
+        data_path="./data",
+        db_file="results.db",
+        links_file_url="https://production-media.paperswithcode.com/about/links-between-papers-and-code.json.gz",
+        batch_size=1000,
+        max_workers=50,
+        lakefs_url="https://lake-bioinfmed.zib.de",
+        lakefs_repo="mardi-workflows-files",
+        lakefs_path_and_file="mardiKG_paper2code_linker/results.db"
+    )
