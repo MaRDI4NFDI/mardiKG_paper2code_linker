ae4cb25a1c3ce0674f774856ac4a21b9915c06e0
Tim Conrad <conrad@zib.de>
2025-04-06 17:31:27 +0200

    Added re-try logic if API call fails

--- workflow_main.py
+++ workflow_main.py
@@ -128,8 +128,8 @@ def process_papers(
 if __name__ == "__main__":
     process_papers(
         links_file_url="https://production-media.paperswithcode.com/about/links-between-papers-and-code.json.gz",
-        batch_size=1000,
-        max_workers=50,
+        batch_size=10,
+        max_workers=3,
         lakefs_url="https://lake-bioinfmed.zib.de",
         lakefs_repo="mardi-workflows-files",
         lakefs_path_and_file="mardiKG_paper2code_linker/results.db"
